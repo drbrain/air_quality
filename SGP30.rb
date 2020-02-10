bc57a4431e84bb7d80cdccd027df7f6d511d9e29
Eric Hodel <drbrain@segment7.net>
2020-02-09 21:14:44 -0800

    Add reading IAQ from the SGP30
    Added read/write transactions to I2C

---
+++ SGP30.rb
@@ -0,0 +1,118 @@
+require_relative "i2c"
+
+class SGP30
+  class Error < RuntimeError
+  end
+
+  # Commands
+
+  IAQ_INIT                    = ["\x20\x03", 0, 0,  10]
+  MEASURE_IAQ                 = ["\x20\x08", 0, 6,  12, "S>S>"]
+  GET_IAQ_BASELINE            = ["\x20\x15", 0, 6,  10, "S>S>"]
+  SET_IAQ_BASELINE            = ["\x20\x1e", 6, 0,  10, "S>S>"]
+  SET_ABSOLUTE_HUMIDITY       = ["\x20\x61", 3, 0,  10, "S>"]
+  MEASURE_TEST                = ["\x20\x32", 0, 3, 220, "S>"]
+  GET_FEATURE_SET             = ["\x20\x2f", 0, 3,  10, "S>"]
+  MEASURE_RAW                 = ["\x20\x50", 0, 6,  25, "S>S>"]
+  GET_TVOC_INCEPTIVE_BASELINE = ["\x20\xb3", 0, 3,  10, "S>"]
+  SET_TVOC_BASELINE           = ["\x20\x77", 3, 0,  10, "S>"]
+
+  def initialize bus, address = 0x58
+    @dev = I2C::Dev.new bus, address
+
+    iaq_init
+
+    set_tvoc_baseline get_tvoc_inceptive_baseline if features == 0x0022
+  end
+
+  def apply_crc data
+    data.bytes.each_slice(2).flat_map { |pair|
+      pair << crc(pair)
+    }.pack "C*"
+  end
+
+  def crc pair
+    pair.reduce(0xFF) { |byte, crc|
+      8.times do
+        if ((byte ^ crc) & 0x80) == 0x80 then
+          crc = (crc << 1) ^ 0x31
+        else
+          crc <<= 1
+        end
+
+        byte <<= 1
+      end
+
+      crc &= 0xFF
+    }
+  end
+
+  def extract_data data
+    data.bytes.each_slice(3).flat_map { |a, b, given_crc|
+      data = [a, b]
+      expected_crc = crc data
+
+      raise Error, "invalid CRC" unless expected_crc == given_crc
+
+      data
+    }.pack "C*"
+  end
+
+  def iaq_init
+    send_command IAQ_INIT
+  end
+
+  def features
+    result = send_command GET_FEATURE_SET
+    result.first
+  end
+
+  def get_tvoc_inceptive_baseline
+    result = send_command GET_TVOC_INCEPTIVE_BASELINE
+    result.first
+  end
+
+  def set_tvoc_baseline baseline
+    send_command SET_TVOC_BASELINE, baseline
+  end
+
+  def measure_iaq
+    send_command MEASURE_IAQ
+  end
+
+  def send_command command, data = nil
+    command, write_length, read_length, max_wait, format = command
+
+    if read_length.zero? then
+      write_command = 
+        if write_length.zero? then
+          command
+        else
+          data = Array(data).pack format
+          data = apply_crc data
+          command + data
+        end
+
+      I2C::Message.write @dev, write_command
+    else
+      I2C::Message.write @dev, command
+
+      sleep max_wait / 1000.0
+
+      message = I2C::Message.read @dev, command, read_length
+      result = message.buffer.to_s read_length
+      data = extract_data result
+      data.unpack format
+    end
+  end
+end
+
+if $0 == __FILE__ then
+  sgp30 = SGP30.new 1
+
+  loop do
+    puts "eCO₂: %5dppm tVOC: %5dppb" % sgp30.measure_iaq
+
+    sleep 1
+  end
+end
--- i2c.rb
+++ i2c.rb
@@ -1,16 +1,36 @@
+require "fiddle"
+require "fiddle/import"
+
 module I2C
+  extend Fiddle::Importer
+
   class Error < RuntimeError; end
+
+  Message = struct [
+    "unsigned short address",
+    "unsigned short flags",
+    "unsigned short length",
+    "char           *buffer",
+  ]
+
+  ReadWriteIOCTLData = struct [
+    "void          *messages",
+    "unsigned long count",
+  ]
 end
 
 class I2C::Dev
-  # linux/i2c.h
+  # uapi/linux/i2c-dev.h
   FOLLOWER = 0x0703
+  RDWR     = 0x0707
 
   class NoSuchBus < I2C::Error
     def initialize bus
       super "No device /dev/i2c-#{bus} found"
     end
   end
+
+  attr_reader :address
 
   def initialize bus, address
     @bus     = bus
@@ -53,7 +73,58 @@ class I2C::Dev
     value.unpack("S<").first
   end
 
+  def transaction message
+    data = I2C::ReadWriteIOCTLData.malloc
+
+    data.messages = message
+    data.count    = 1
+
+    @i2c.ioctl RDWR, data.to_ptr
+  end
+
   def write data
-    @i2c.syswrite data.pack "C*"
+    @i2c.syswrite Array(data).pack "C*"
   end
 end
+
+class I2C::Message
+  #
+  # uapi/linux/i2c.h
+
+  # i2c_msg
+  WRITE = 0x0000 # M_WR
+  READ  = 0x0001 # M_RD
+
+  attr_writer :device
+
+  def self.read device, data, length
+    buffer = Fiddle::Pointer.malloc length
+    buffer[0, data.bytesize] = data
+
+    message = malloc
+
+    message.address = device.address
+    message.flags   = READ
+    message.length  = length
+    message.buffer  = buffer
+
+    device.transaction message
+    message
+  end
+
+  def self.write device, data
+    buffer = Fiddle::Pointer.malloc data.bytesize
+    buffer[0, data.bytesize] = data
+
+    message = malloc
+
+    message.address = device.address
+    message.flags   = WRITE
+    message.length  = data.bytesize
+    message.buffer  = buffer
+
+    device.transaction message
+    message
+  end
+end
+
