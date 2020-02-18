4a690c7b640e3c163bb1614b7839e298dbb155b7
Eric Hodel <drbrain@segment7.net>
2020-02-17 16:07:57 -0800

    Add bus scanning based on i2cdetect

--- i2c.rb
+++ i2c.rb
@@ -6,6 +6,19 @@ module I2C
 
   class Error < RuntimeError; end
 
+  SMBUS_READ  = 1
+  SMBUS_WRITE = 0
+
+  SMBUS_QUICK = 0
+  SMBUS_BYTE  = 1
+
+  IOCTLData = struct [
+    "unsigned short read_write",
+    "unsigned short command",
+    "unsigned long  size",
+    "void           *data",
+  ]
+
   Message = struct [
     "unsigned short address",
     "unsigned short flags",
@@ -17,12 +30,39 @@ module I2C
     "void          *messages",
     "unsigned long count",
   ]
+
+  SMBusData = union [
+    "unsigned char  byte",
+    "unsigned short word",
+    "unsigned char  *block",
+  ]
+
+  def self.scan bus, first: 0x03, last: 0x77
+    return enum_for __method__, bus, first: first, last: last unless
+      block_given?
+
+    (first..last).map do |address|
+      dev = I2C::Dev.new bus, address
+
+      case address
+      when 0x30..0x37,
+           0x50..0x5F then
+        dev.read_byte
+      else
+        dev.write_quick 0
+      end
+
+      yield dev
+    rescue Errno::EREMOTEIO
+    end
+  end
 end
 
 class I2C::Dev
   # uapi/linux/i2c-dev.h
   FOLLOWER = 0x0703
   RDWR     = 0x0707
+  SMBUS    = 0x0720
 
   class NoSuchBus < I2C::Error
     def initialize bus
@@ -52,6 +92,14 @@ class I2C::Dev
     @i2c.sysread length
   end
 
+  def read_byte
+    data = I2C::SMBusData.malloc
+
+    smbus_access I2C::SMBUS_READ, 0, I2C::SMBUS_BYTE, data
+
+    data.byte & 0xFF
+  end
+
   def read_S8 register
     value = read register, 1
 
@@ -73,6 +121,17 @@ class I2C::Dev
     value.unpack("S<").first
   end
 
+  def smbus_access read_write, command, size, data
+    args = I2C::IOCTLData.malloc
+
+    args.read_write = read_write
+    args.command    = command
+    args.size       = size
+    args.data       = data
+
+    err = @i2c.ioctl SMBUS, args.to_ptr
+  end
+
   def transaction message
     data = I2C::ReadWriteIOCTLData.malloc
 
@@ -84,6 +143,10 @@ class I2C::Dev
 
   def write data
     @i2c.syswrite Array(data).pack "C*"
+  end
+
+  def write_quick value
+    smbus_access value, 0, I2C::SMBUS_QUICK, nil
   end
 end
 
